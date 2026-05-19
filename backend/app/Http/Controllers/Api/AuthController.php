0f7067aea99c31164f17822229b54dcfe3db02b0
DNishadini <dinushikaherath150@gmail.com>
2026-05-19 19:47:00 +0530

    add dashboard features and auth backend

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function register(
+        Request $request
+    ) {
+
+        $user =
+            User::create([
+
+                'name' =>
+
+                $request->name,
+
+                'email' =>
+
+                $request->email,
+
+                'password' =>
+
+                Hash::make(
+                    $request->password
+                )
+
+            ]);
+
+        return response()->json(
+            $user
+        );
+    }
+
+    public function login(
+        Request $request
+    ) {
+
+        $user = User::where(
+
+            'email',
+
+            $request->email
+
+        )->first();
+
+        if (
+
+            !$user ||
+
+            !Hash::check(
+
+                $request->password,
+
+                $user->password
+
+            )
+
+        ) {
+
+            return response()->json([
+
+                "message" =>
+
+                "Invalid credentials"
+
+            ], 401);
+        }
+
+        return response()->json([
+
+            "message" =>
+
+            "Login successful",
+
+            "user" =>
+
+            $user
+
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,8 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TodoController;
+use App\Http\Controllers\Api\AuthController;
+
 
 Route::apiResource(
     'todos',
     TodoController::class
+);
+
+Route::post(
+    '/register',
+
+    [AuthController::class, 'register']
+
+);
+
+Route::post(
+    '/login',
+
+    [AuthController::class, 'login']
+
 );
